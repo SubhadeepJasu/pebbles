103f7d191807d14ed46cc0a5718db95bee49d3ad
SubhadeepJasu <subhadeep107@proton.me>
2025-04-18 00:38:39 +0530

    Add Date add/subtraction functionality

--- src/core/date_calculator.py
+++ src/core/date_calculator.py
@@ -3,9 +3,10 @@
 # SPDX-FileCopyrightText: 2024 Subhadeep Jasu <subhadeep107@proton.me>
 # SPDX-FileCopyrightText: 2020 Saunak Biswas <saunakbis97@gmail.com>
 
-"""Converter"""
+"""Date Calculator"""
 
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from gi.repository import GLib
 
 class DateCalculator:
@@ -13,7 +14,7 @@ class DateCalculator:
     Date calculator.
     """
 
-    def evaluate_difference (self, from_date:GLib.DateTime, to_date:GLib.DateTime):
+    def evaluate_difference(self, from_date:GLib.DateTime, to_date:GLib.DateTime):
         """
         Find the difference between  two given dates in days
         """
@@ -26,3 +27,47 @@ class DateCalculator:
         ).date()
 
         return str((_to_date - _from_date).days)
+
+
+    def add_to_date(self,
+                    start:GLib.DateTime,
+                    days_to_add:int,
+                    months_to_add:int,
+                    years_to_add:int
+    ):
+        """
+        Add certain days, months and year to a given date to get a new date.
+        """
+
+        _start_date = datetime.fromtimestamp(start.to_unix()).replace(
+            hour=0, minute=0, second=0, microsecond=0
+        ).date()
+
+        new_date = _start_date + relativedelta(
+            days=days_to_add,
+            months=months_to_add,
+            years=years_to_add
+        )
+        return GLib.Date.new_dmy(new_date.day, new_date.month, new_date.year)
+
+
+    def subtract_from_date(self,
+                           start:GLib.DateTime,
+                           days_to_add:int,
+                           months_to_add:int,
+                           years_to_add:int
+    ):
+        """
+        Subtract certain days, months and year from a given date to get a new date.
+        """
+
+        _start_date = datetime.fromtimestamp(start.to_unix()).replace(
+            hour=0, minute=0, second=0, microsecond=0
+        ).date()
+
+        new_date = _start_date - relativedelta(
+            days=days_to_add,
+            months=months_to_add,
+            years=years_to_add
+        )
+        return GLib.Date.new_dmy(new_date.day, new_date.month, new_date.year)
